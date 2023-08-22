7cbbe1f82e623debd65e217147e55a232cbef64c
Jaidi <83307221+Jdparra2004@users.noreply.github.com>
2023-08-22 07:51:00 -0500

    parcial1_juan_parra.py

--- parcial1_juan_parra.py
+++ parcial1_juan_parra.py
@@ -9,5 +9,20 @@ Created on Agust 22 at 6:09 am
 
 #Problema 3
 
-n = (3, 5, 10, 50)
+import math
+import matplotlib
+import numpy as np
+
+#Tener en cuenta que la frunción corresponde a una serie de Taylor
+
+
+def exponencial(x):
+    n = 3
+    sumatoria = 0
+    
+    for n in range(0, n, 1):
+        sumatoria +=  math.pow(x,n) / math.factorial(n)
+    return sumatoria
+print(f"El resultado con la función exponencial es {exponencial(2)}")
+print(f"El resultado con la función math.exp es {math.exp(2)}")
 
