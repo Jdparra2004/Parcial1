b522e09cb75e63dca296ccbdacda97485412a5c6
Jaidi <83307221+Jdparra2004@users.noreply.github.com>
2023-08-22 07:08:13 -0500

    Add files via upload

---
+++ problema2.py
@@ -0,0 +1,35 @@
+'''
+Solución Problema 1 - Quiz y Parcial
+
+Created on Agust 22 at 6:12 am
+
+@authot: Juan David Parra Cantor
+
+'''
+
+import numpy as np 
+from numpy import random
+
+
+#variables y datos
+n = 1
+m = n*2
+m = int(m)
+a = 1
+
+v = [1, 7, -5, 0, 7, 2, 9, 7]
+print(v)
+
+i = n
+
+
+if i == n:
+    i += 1
+    i = a
+    print(f"el número n = {n}, se encuentra en la posición: {i, n-1}")
+else: 
+    print(f"El número {n}, no se encuentra en el vector")
+
+
+
+
